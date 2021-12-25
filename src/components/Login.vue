a3e206bb8ef41a37ee1014e45de586c8813844f6
YEAN SEYHA <yeanseyha7@gmail.com>
2021-12-25 13:22:36 +0700

    Adding routes

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Log in page</h1>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is log in page</h1>
+    <login></login>
+  </div>
+</template>
+
+<script>
+
+import Login from '../components/Login.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Login
+  }
+}
+</script>
